bac33751e4b04eeff1a2dfd472ab469e9cbe9fcb
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2022-11-11 21:37:54 -0300

    :lipstick: posible error de ruta

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
